d345747b594b9a589e809668e11800add2209f2c
ac2pic <ac2pic@gmail.com>
2023-01-27 01:28:24 -0600

    Fix kernel panic bug

--- Internal_PKG_Installer/util.cpp
+++ Internal_PKG_Installer/util.cpp
@@ -1,5 +1,19 @@
 #include "util.h"
 
+
+
+void run_as_root(void * args) {
+	struct VirtualRunArgs * vrArgs = (struct VirtualRunArgs*)args;
+	vrArgs->returnValue = vrArgs->funcPtr();
+}
+
+int sudo_func(int (*funcPtr)()) {
+	struct VirtualRunArgs args;
+	args.funcPtr = funcPtr;
+	jbc_run_as_root(run_as_root, &args, CWD_ROOT);
+	return args.returnValue;
+}
+
 int system_notification(const char* text, const char* iconName) {
 	OrbisNotificationRequest NotificationBuffer;
 	
